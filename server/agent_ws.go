2a2dff3aa7fea4fc6e629ac9eea4268fbc4021b4
covuaduongsinh <ductuonghn@gmail.com>
2026-09-20 23:43:49 +0700

    debug: log initial configOptions payload temporarily

--- server/agent_ws.go
+++ server/agent_ws.go
@@ -228,7 +228,9 @@ func (s *Server) handleAgentWS(w http.ResponseWriter, r *http.Request) {
 	session.SubscribeUpdates(sessCtx, sub)
 
 	_ = wsjson.Write(sessCtx, conn, agentServerMessage{Type: "ready", SessionID: session.SessionID})
-	if opts := session.ConfigOptions(); len(opts) > 0 {
+	opts := session.ConfigOptions()
+	log.Printf("agent_ws: DEBUG initial configOptions count=%d raw=%+v", len(opts), opts)
+	if len(opts) > 0 {
 		_ = wsjson.Write(sessCtx, conn, agentServerMessage{Type: "configOptions", Data: opts})
 	}
 
